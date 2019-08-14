65ae01b4d95ac7501fb808daca6a897095d62245
bl1n <sukhanov87@gmail.com>
2019-08-14 14:50:50 +0300

    works

--- app/src/main/java/team/lf/cryptoapp/formatters/Ext.kt
+++ app/src/main/java/team/lf/cryptoapp/formatters/Ext.kt
@@ -1,2 +1,17 @@
 package team.lf.cryptoapp.formatters
 
+import java.text.SimpleDateFormat
+import java.util.*
+
+fun Float.formatThousands() : String {
+    val sb = StringBuilder()
+    val formatter = Formatter(sb, Locale.US)
+    formatter.format("%(,.0f", this)
+    return sb.toString()
+}
+
+fun Number.dateToString(pattern: String): String {
+    val calendar = Calendar.getInstance()
+    calendar.timeInMillis = this.toLong()
+    return SimpleDateFormat(pattern).format(calendar.time)
+}
